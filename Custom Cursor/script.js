848be51a9a8d72e89d507593a196e1bc25c4b10e
Sagarika Padhy <88076571+Gudli@users.noreply.github.com>
2024-02-03 16:18:27 +0530

    Add files via upload

---
+++ Custom Cursor/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+var main = document.querySelector("#main")
+var crsr = document.querySelector(".curser")
+
+main.addEventListener("mousemove",function(dets){
+    crsr.style.left = dets.x+"px"
+    crsr.style.top = dets.y+"px"
+})
+})
